165717c8c525b359e56c19dc407f88fe3da94e4c
Mern2025 <merndeveloper2025@gmail.com>
2025-07-21 03:34:29 +0600

    just try

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// let div = document.createElement('div')
+
+// div.className = 'main'
+// // div.setAttribute('title', 'generated title')
+
+// div.style.background = 'green'
+// div.padding = '20px'
+// div.innerHTML = 'ruhul'
+
+// document.body.appendChild(div)
+
+// // console.log(div)
+
+
+
+// let main1 = document.querySelector('.main1')
+// main1.style.background = 'green'
+
+// let div = document.createElement('div')
+
+// div.className = 'content_box'
+
+// div.innerHTML= 'hi ruhul'
+
+// main1.appendChild(div)
+
+// let banner = document.querySelector('.banner')
+
+// let div = document.createElement('div')
+// let content = document.createElement('div')
+// div.className = 'image_part'
+// div.innerHTML = 'hi'
+
+// content.className = 'content_section'
+
+// banner.appendChild(content)
+
+// banner.appendChild(div)
+
+
+
